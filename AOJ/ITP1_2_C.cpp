2de4d51172d7466e0512eded4f149d0225f98b8c
oguri51 <129643168+oguri51@users.noreply.github.com>
2023-04-04 21:02:49 +0900

    Update ITP1_2_C.cpp
    use sort

--- AOJ/ITP1_2_C.cpp
+++ AOJ/ITP1_2_C.cpp
@@ -1,25 +1,18 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 int main() {
     
-    int a, b, c, tmp;
+    int a, b, c;
 
     cin >> a >> b >> c;
 
     vector<int> num = {a, b, c};
 
-    for(int i=0; i < num.size() - 1; i++){
-        for(int j=1; j < num.size(); j++){
-            if(num[i] > num[j]){
-                tmp = num[i];
-                num[i] = num[j];
-                num[j] = tmp;
-            } 
-        }
-    }
-    
+    sort(num.begin(), num.end());
+
     cout << num[0];
     for(int i=1; i < num.size(); i++) cout << " " << num[i];
     cout << endl;

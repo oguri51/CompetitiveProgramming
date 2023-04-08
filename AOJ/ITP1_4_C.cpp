7ee2d6f908f6fdb15fbaa00f412d38ee2a78504e
oguri51 <129643168+oguri51@users.noreply.github.com>
2023-04-08 14:51:52 +0900

    2023/04/08 ITP1_4_C
    Simple Calculator

---
+++ AOJ/ITP1_4_C.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int a, b;
+    char op;
+
+    while(1){
+        cin >> a >> op >> b;
+
+        if(op == '?') break;
+
+        switch(op){
+            case '+':
+                cout << a + b << endl;
+                break;
+            case '-':
+                cout << a - b << endl;
+                break;
+            case '*':
+                cout << a * b << endl;
+                break;
+            case '/':
+                cout << a / b << endl;
+                break;
+        }
+    }
+
+    return 0;
+}
